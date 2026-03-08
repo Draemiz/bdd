c9cef76daac5ff9013d8e285d91ffee2afac3781
Draemiz <draemizgx1@gmail.com>
2026-03-08 00:26:09 -0500

    en la carpeta inserciones se agrego 5 inserts en el archivo productos y se ejecuto 3 inserts, luego en el archivo cuentas se creo y ejecuto 10 inserts

--- inserciones/productos.sql
+++ inserciones/productos.sql
@@ -9,3 +9,20 @@ create table productos(
 	stock int not null,
     constraint productos_pk primary key(codigo)
 );
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values ('00015','Cereal','Cereal de chocolate para niños',3.60,30);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values ('00016','Leche','Leche entera 1 litro',1.20,50);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values ('00017','Pan','Pan blanco fresco',0.80,40);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values ('00018','Galletas','Galletas dulces rellenas',2.10,25);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values ('00019','Jugo','Jugo de naranja natural',1.50,35);
+
+select codigo, nombre, descripcion,precio,stock from productos;
--- scripts/personas.sql
+++ scripts/personas.sql
@@ -12,3 +12,19 @@ create table personas(
     numero_hijos int,
     constraint personas_pk primary key(cedula)
 );
+
+insert into personas(cedula, nombre, apellido)
+values ('1785326581','Alexandra','Moreno');
+
+insert into personas(cedula, nombre, apellido,estatura)
+values ('1785536581','Santiago','Mosquera',1.85);
+
+insert into personas(cedula, nombre, apellido,numero_hijos)
+values ('1785548381','Pilar','Mayacela',3);
+
+insert into personas(cedula, nombre, apellido,numero_hijos,estatura,fecha_nacimiento,hora_nacimiento,cantidad_ahorrada)
+values ('1716678388','Tania','Ramirez',1,1.80,'13/06/1993','22:54',222.50);
+
+select cedula, nombre, numero_hijos from personas;
+
+select * from personas;
