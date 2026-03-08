56ead127d3cb146db9a743067c257b2f87b628d9
Draemiz <draemizgx1@gmail.com>
2026-03-08 00:48:20 -0500

    8 inserts para la tabla registros_entrada y 10 para la tabla de estudiantes fueron creados

--- inserciones/registros_entrada.sql
+++ inserciones/registros_entrada.sql
@@ -8,3 +8,35 @@ create table registros_entrada(
 	hora time not null,
     constraint registros_entrada_pk primary key(codigo_registro)
 );
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (1,'1714616123','01/06/2024','08:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (2,'1785326581','02/06/2024','08:05');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (3,'0923456789','03/06/2024','07:55');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (4,'1102567894','04/06/2024','08:10');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (5,'1712345678','05/06/2024','08:02');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (6,'0956781234','06/06/2024','07:59');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (7,'1723456789','07/06/2024','08:15');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (8,'0965432187','08/06/2024','08:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (9,'1719988776','09/06/2024','08:07');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values (10,'1781112233','10/06/2024','08:03');
+
+select codigo_registro, cedula_empleado, fecha, hora from registros_entrada;
