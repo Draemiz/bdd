4eb36b314a7201f3710f0529ee028a74bee21b9b
Draemiz <draemizgx1@gmail.com>
2026-03-15 12:57:26 -0500

    selects agregados al archivo transacciones y modificado los scripts

--- Selecciones2/transacciones.sql
+++ Selecciones2/transacciones.sql
@@ -12,51 +12,41 @@ create table transacciones(
 );
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (1,'00001',200.00,'C','01/03/2024','08:10');
+values (1,'22001',200.00,'C','2024-05-20','08:10');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (2,'00002',150.50,'D','02/08/2024','16:15');
+values (2,'22002',150.50,'D','2024-05-29','16:15');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (3,'00003',500.00,'C','03/07/2024','10:20');
+values (3,'22003',500.00,'C','2024-05-10','10:20');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (4,'00004',75.25,'D','04/04/2024','11:05');
+values (4,'22009',75.25,'D','2024-05-25','11:05');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (5,'00005',320.00,'C','05/09/2024','12:30');
+values (5,'22005',320.00,'C','2024-05-26','12:30');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (6,'00006',40.00,'D','06/02/2024','13:45');
+values (6,'22006',40.00,'D','2024-05-29','13:45');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (7,'00007',890.10,'C','07/01/2024','14:10');
+values (7,'22007',890.10,'C','2024-05-15','14:10');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (8,'00008',60.75,'D','08/06/2024','15:25');
+values (8,'22008',60.75,'D','2024-05-18','15:25');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (9,'00009',1200.00,'C','09/06/2024','16:40');
+values (9,'22012',1200.00,'C','2024-05-26','16:40');
 
 insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
-values (10,'00010',95.90,'D','10/01/2024','17:55');
+values (10,'22010',95.90,'D','2024-05-29','17:55');
+
+select * from transacciones where tipo = 'C'
+and numero_cuenta between '22001' and '22004';
 
 select * from transacciones
-where tipo = 'D';
+where tipo = 'D' and fecha = '2024-05-25' and numero_cuenta between '22007' and '22010';
 
 select * from transacciones
-where monto between 200::money and 2000::money;
-
-select codigo, monto, tipo, fecha from transacciones
-where fecha is not null;
-
-select * from transacciones
-update transacciones set tipo = 'D'
-where monto > 100::money and monto < 500::money
-and fecha between '2024-09-01' and '2024-09-30'
-and hora between '14:00' and '20:00';
-
-delete from transacciones
-where tipo = 'D'
-and hora between '14:00' and '18:00'
-and fecha between '2024-08-01' and '2024-08-31';
+where codigo between 1 and 5 and (numero_cuenta = '22002' or numero_cuenta = '22004')
+and fecha in ('2024-05-26','2024-05-29');
