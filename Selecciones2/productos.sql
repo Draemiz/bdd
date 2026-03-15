a65c75fef449dfc37857d0f580c4fd6d346bffd7
Draemiz <draemizgx1@gmail.com>
2026-03-15 12:17:04 -0500

    selects agregados al archivo productos y modificado los scripts

--- Selecciones2/productos.sql
+++ Selecciones2/productos.sql
@@ -10,20 +10,30 @@ create table productos(
     constraint productos_pk primary key(codigo)
 );
 
-insert into productos(codigo, nombre, descripcion,precio)
-values ('00015','Cereal','Cereal de chocolate para niños',3.60);
-
-insert into productos(codigo, nombre,precio)
-values ('00016','Leche',1.20);
-
 insert into productos(codigo, nombre, descripcion,precio,stock)
-values ('00017','Pan','Pan blanco sin bordes',0.80,40);
-
-insert into productos(codigo, nombre, descripcion,precio,stock)
-values ('00018','Galletas','Galletas chips dulces rellenas',2.10,25);
-
-insert into productos(codigo, nombre, descripcion,precio,stock)
-values ('00019','Jugo','Jugo de naranja natural',1.50,35);
+values (15,'cereal','cereal de chocolate para niños',3.60,10);
 
 insert into productos(codigo, nombre,precio,stock)
-values ('00020','Queso',2.50,40);
+values (16,'leche',1.20,10);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values (17,'pan','pan blanco sin bordes',0.80,40);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values (18,'galletas','galletas chips dulces rellenas',2.10,25);
+
+insert into productos(codigo, nombre, descripcion,precio,stock)
+values (19,'jugo','jugo de naranja natural',1.50,0);
+
+insert into productos(codigo, nombre,precio,stock)
+values (20,'queso',2.50,40);
+
+select * from productos
+where stock = 10 and precio < 10::money;
+
+select nombre, stock, descripcion from productos
+where nombre like '%m%' or descripcion like '% %';
+
+select * from productos
+where descripcion is null or stock = 0;
+
