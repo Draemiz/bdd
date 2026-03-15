70c0ca1d7c3b73d70679e86ea59adf78f764043f
Draemiz <draemizgx1@gmail.com>
2026-03-15 12:45:53 -0500

    selects agregados al archivo VIDEOJUEGOS y modificado los scripts

--- Selecciones2/registros_entrada.sql
+++ Selecciones2/registros_entrada.sql
@@ -10,32 +10,45 @@ create table registros_entrada(
 );
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (1,'1714616123','01/06/2024','08:00');
+values (1,'1714616123','2024-06-01','08:00');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (2,'1785326581','02/09/2024','08:05');
+values (2,'1785326581','2024-09-02','08:05');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (3,'0923456789','03/08/2024','07:55');
+values (3,'0823456789','2024-08-03','09:30');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (4,'1102567894','04/11/2024','08:10');
+values (4,'1102567894','2024-11-04','08:10');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (5,'1712345678','05/06/2024','08:02');
+values (5,'1712345678','2024-06-05','08:02');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (6,'0956781234','06/02/2024','07:59');
+values (6,'0956781234','2024-02-06','07:59');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (7,'1723456789','07/08/2024','08:15');
+values (7,'1723456789','2024-08-07','08:15');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (8,'0965432187','08/04/2024','08:00');
+values (8,'0865432187','2024-09-08','10:00');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (9,'1719988776','09/05/2024','08:07');
+values (9,'1719988776','2024-05-09','08:07');
 
 insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
-values (10,'1781112233','10/01/2024','08:03');
+values (10,'1781112233','2024-01-10','08:03');
 
+select * from registros_entrada
+where fecha between '01/09/2024' and '30/09/2024'
+or cedula_empleado like '17%';
+
+select * from registros_entrada
+where fecha between '01/08/2024' and '31/08/2024'
+and cedula_empleado like '17%' and hora between '08:00' and '12:00';
+
+select * from registros_entrada
+where (fecha between '01/08/2024' and '31/08/2024' and cedula_empleado like '17%'
+    and hora between '08:00' and '12:00')
+or (fecha between '01/09/2024' and '30/09/2024' and cedula_empleado like '08%'
+    and hora between '09:00' and '13:00');
--- Selecciones2/videojuegos.sql
+++ Selecciones2/videojuegos.sql
@@ -18,21 +18,29 @@ insert into videojuegos(codigo, nombre, valoracion)
 values (2,'Minecraft',9);
 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values (3,'God of War','Aventura de acción basada en mitología nórdica',10);
+values (3,'God of War','Juego de guerra basado en mitología nórdica',10);
 
 insert into videojuegos(codigo, nombre, valoracion)
 values (4,'FIFA 24',5);
 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values (5,'Call of Duty','Juego de disparos en primera persona',9);
-
---solo obligatorios
+values (5,'Call of Duty','Juego de guerra en primera persona',9);
 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values (6,'Fortnite','Juego battle royale multijugador',8);
+values (6,'Fortnite','Juego battle royale multijugador',7);
 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
 values (7,'Zelda Breath of the Wild','Aventura de exploración en mundo abierto',10);
 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values (8,'Grand Theft Auto V','Juego de acción en mundo abierto',9);
+values (8,'Doom Eternal','Juego de guerra contra demonios',9);
+
+select * from videojuegos
+where nombre like '%c%' or valoracion = 7;
+
+select * from videojuegos
+where codigo between 3 and 7 or valoracion = 7;
+
+select * from videojuegos 
+where (descripcion like '%guerra%' and valoracion > 7 and nombre like 'C%')
+or (valoracion > 8 and nombre like 'D%');
